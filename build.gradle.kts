a0570244dcaf31909df00b69b743f5604b3fb9a3
Cubxity <contact@cubxity.dev>
2021-01-24 18:11:29 +0100

    Github workflows

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,7 +21,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     kotlin("jvm") version "1.4.21"
-    id("com.github.johnrengelman.shadow") version "6.1.0"
+    id("com.github.johnrengelman.shadow") version "6.1.0" apply false
 }
 
 allprojects {
