0e18bb11a666952ef0b827f31705a6ddbb97f3d8
Cubxity <cubxity@protonmail.com>
2021-02-22 13:08:01 +0100

    Make use of NMS or Paper for TPS + added MSPT metric

--- bukkit/src/main/kotlin/dev/cubxity/plugins/metrics/bukkit/metric/tps/PaperTPSProvider.kt
+++ bukkit/src/main/kotlin/dev/cubxity/plugins/metrics/bukkit/metric/tps/PaperTPSProvider.kt
@@ -16,37 +16,13 @@
  *     along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
 
-import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
-import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+package dev.cubxity.plugins.metrics.bukkit.metric.tps
 
-plugins {
-    kotlin("jvm") version "1.4.30"
-    kotlin("kapt") version "1.4.30"
-    id("com.github.johnrengelman.shadow") version "6.1.0" apply false
-}
+import org.bukkit.Bukkit
 
-allprojects {
-    group = "dev.cubxity.plugins"
-    description = "Fully featured metrics collector agent for Minecraft servers."
-    version = "0.1.2"
-
-    repositories {
-        mavenCentral()
-    }
-}
-
-subprojects {
-    apply(plugin = "java")
-    apply(plugin = "kotlin")
-    apply(plugin = "kotlin-kapt")
-    apply(plugin = "com.github.johnrengelman.shadow")
-
-    tasks.withType<KotlinCompile> {
-        kotlinOptions {
-            jvmTarget = "1.8"
-        }
-    }
-    tasks.withType<ShadowJar> {
-        archiveClassifier.set("")
-    }
+class PaperTPSProvider : TPSProvider {
+    override val tps: Double
+        get() = Bukkit.getServer().tps[0]
+    override val mspt: Long
+        get() = Bukkit.getServer().tickTimes[0]
 }
--- bukkit/src/main/kotlin/dev/cubxity/plugins/metrics/bukkit/util/NMSUtil.kt
+++ bukkit/src/main/kotlin/dev/cubxity/plugins/metrics/bukkit/util/NMSUtil.kt
@@ -16,37 +16,7 @@
  *     along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
 
-import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
-import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+package dev.cubxity.plugins.metrics.bukkit.util
 
-plugins {
-    kotlin("jvm") version "1.4.30"
-    kotlin("kapt") version "1.4.30"
-    id("com.github.johnrengelman.shadow") version "6.1.0" apply false
-}
-
-allprojects {
-    group = "dev.cubxity.plugins"
-    description = "Fully featured metrics collector agent for Minecraft servers."
-    version = "0.1.2"
-
-    repositories {
-        mavenCentral()
-    }
-}
-
-subprojects {
-    apply(plugin = "java")
-    apply(plugin = "kotlin")
-    apply(plugin = "kotlin-kapt")
-    apply(plugin = "com.github.johnrengelman.shadow")
-
-    tasks.withType<KotlinCompile> {
-        kotlinOptions {
-            jvmTarget = "1.8"
-        }
-    }
-    tasks.withType<ShadowJar> {
-        archiveClassifier.set("")
-    }
-}
+fun getNMSClass(name: String): Class<*> =
+    Class.forName("net.minecraft.server.${Environment.serverApiVersion}.$name")
