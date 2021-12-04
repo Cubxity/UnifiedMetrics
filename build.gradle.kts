9a90bbb61b9523ab636caecf63c6b12d383a559a
Cubxity <cubxity@protonmail.com>
2021-12-04 20:43:34 +0100

    chore: bump version to 0.3.4

--- build.gradle.kts
+++ build.gradle.kts
@@ -28,7 +28,7 @@ plugins {
 allprojects {
     group = "dev.cubxity.plugins"
     description = "Fully featured metrics collector agent for Minecraft servers."
-    version = "0.3.4-SNAPSHOT"
+    version = "0.3.4"
 
     repositories {
         mavenCentral()
