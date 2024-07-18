d5f466a76037a8a282caec0a5bb6bf100ec44ab8
Cubxity <cubxity@protonmail.com>
2024-07-18 17:59:35 +0200

    build(bukkit): set mappings namespace to skip remapping on Paper 1.20.5+

--- platforms/bukkit/build.gradle.kts
+++ platforms/bukkit/build.gradle.kts
@@ -37,6 +37,10 @@ tasks {
         relocate("okhttp", "dev.cubxity.plugins.metrics.libs.okhttp")
         relocate("okio", "dev.cubxity.plugins.metrics.libs.okio")
         relocate("io.prometheus", "dev.cubxity.plugins.metrics.libs.io.prometheus")
+
+        manifest {
+            attributes(mapOf("paperweight-mappings-namespace" to "mojang"))
+        }
     }
     processResources {
         filesMatching("plugin.yml") {
